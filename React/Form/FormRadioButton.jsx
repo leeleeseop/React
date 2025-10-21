3da033cda1f5c77b1ab3175f55f6a8baf9713992
leeleeseop <iseobi138@gmail.com>
2025-10-21 23:26:00 +0900

    Form radio button

---
+++ React/Form/FormRadioButton.jsx
@@ -0,0 +1,36 @@
+import {useState} from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+   const [flower, setFlower] = useState("장미꽃");
+   
+   const handleChange = (e) => {
+    setFlower(e.target.value);
+   }
+
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    let result = `당신이 고른 꽃은 ${flower} 입니다.`;
+    alert(result);
+   }
+
+   return (
+    // checked={flower === '장미꽃'}: 현재 state와 value가 일치하면 true(선택됨), 아니면 false(선택 해제)
+    <form onSubmit={handleSubmit}>
+        <label>꽃을 골라 주세요.</label>
+        <br />
+        <label>
+            <input type="radio" name="flower" value="장미꽃" checked={flower == '장미꽃'} onChange={handleChange} /> 장미꽃
+        </label>
+        <label>
+            <input type="radio" name="flower" value="국화" checked={flower == '국화'} onChange={handleChange} /> 국화
+        </label>
+        <label>
+            <input type="radio" name="flower" value="코스모스" checked={flower == '코스모스'} onChange={handleChange} /> 코스모스
+        </label>      
+        <button>Submit</button>          
+    </form>
+   )
+}
+
+createRoot(document.getElementById('root')).render(<App />);
